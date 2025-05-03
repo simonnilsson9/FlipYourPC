94cfa7dd9c233bea444e4026a1bdab991dadcd71
Simon Nilsson <simon.nilsson.10@gmail.com>
2025-05-03 23:59:36 +0200

    feature/PCFLIPPING-21 - Configured template for user. Also implemented basic login-logic with Jwt-bearer token.

---
+++ FlipYourPC/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using FlipYourPC.Entities;
+using FlipYourPC.Models.DTO;
+
+namespace FlipYourPC.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<User?> RegisterAsync(UserDTO request);
+        Task<TokenResponseDTO?> LoginAsync(UserDTO request);
+        Task<TokenResponseDTO?> RefreshTokensAsync(RefreshTokenRequestDTO request);
+    }
+}
